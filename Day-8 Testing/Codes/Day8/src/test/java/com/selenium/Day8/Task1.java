8abda1cf69a76caf70d8fe0dde5d8e03ddb31f7d
Arunkrishna E S <arunkrishnaips2003@gmail.com>
2023-06-05 11:30:56 +0530

    testing

---
+++ Day-8 Testing/Codes/Day8/src/test/java/com/selenium/Day8/Task1.java
@@ -0,0 +1,32 @@
+package com.selenium.Day8;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Task1 {
+
+	public static void main(String[] args) throws InterruptedException 
+	{
+		WebDriverManager.chromedriver().setup();
+    	
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("https://demoqa.com/droppable/");
+		
+		Actions a=new Actions(driver);
+		WebElement source_ele=driver.findElement(By.id("draggable"));
+		WebElement dest_ele=driver.findElement(By.id("droppable"));
+		
+		a.clickAndHold(source_ele).release(dest_ele).build().perform();
+		Thread.sleep(5000);
+		driver.quit();
+
+	}
+
+}
